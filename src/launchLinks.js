05a79baac313cda5df8ace967cff3e3c90c82e25
Alexandra Kenin <alexandra@urbanhikersf.com>
2026-08-24 22:02:33 -0700

    Use permanent launch page URLs

--- src/launchLinks.js
+++ src/launchLinks.js
@@ -1,6 +1,6 @@
 export const LAUNCH_LINKS = {
-  privacy: 'https://sf-stairway-spotter.vercel.app/privacy',
-  terms: 'https://sf-stairway-spotter.vercel.app/terms',
-  support: 'https://sf-stairway-spotter.vercel.app/support',
-  deleteAccount: 'https://sf-stairway-spotter.vercel.app/delete-account',
+  privacy: 'https://www.sfstairwayspotter.com/privacy',
+  terms: 'https://www.sfstairwayspotter.com/terms',
+  support: 'https://www.sfstairwayspotter.com/support',
+  deleteAccount: 'https://www.sfstairwayspotter.com/delete-account',
 };
