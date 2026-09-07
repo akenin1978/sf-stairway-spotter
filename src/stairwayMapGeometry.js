52b21ffa84580b7d27ba7ea56889ca098eafc1bc
Alexandra Kenin <alexandra@urbanhikersf.com>
2026-09-07 08:12:59 -0700

    Move overgrown Buena Vista stairway pin

--- src/stairwayMapGeometry.js
+++ src/stairwayMapGeometry.js
@@ -275,7 +275,7 @@ const STAIRWAY_MAP_GEOMETRY = new Map([
         { lat: 37.76831, lng: -122.442342 },
         { lat: 37.76838, lng: -122.44234 },
       ],
-      markerPosition: { lat: 37.76825, lng: -122.442315 },
+      markerPosition: { lat: 37.7683, lng: -122.44212 },
     },
   ],
   [
