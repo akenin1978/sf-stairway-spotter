8ef558da422fe8e23588cc4b222601be7ee39201
Alexandra Kenin <alexandra@urbanhikersf.com>
2026-09-06 22:39:11 -0700

    Extend Buena Vista Stairway 8 north

--- src/buenaVistaMapGeometry.test.js
+++ src/buenaVistaMapGeometry.test.js
@@ -198,6 +198,7 @@ describe('Buena Vista Park stairway map geometry', () => {
       getStairwayMapGeometry('4e855dcb-d2cd-4158-9e83-f5398ac3822f')
     ).toEqual({
       path: [
+        { lat: 37.76877, lng: -122.44123 },
         { lat: 37.76849, lng: -122.441335 },
         { lat: 37.76845, lng: -122.44135 },
         { lat: 37.76835, lng: -122.44164 },
